c6829b93c6e379de69ed14dc947868dac3253f84
Brindtha <jbrindtha@gmail.com>
2021-12-10 02:01:12 -0800

    login branch created

---
+++ src/page/loginpage.java
@@ -0,0 +1,11 @@
+package page;
+
+public class loginpage {
+
+	public void login() {
+
+		System.out.println("login sucess");
+
+	}
+
+}
---
+++ src/testpage/logintestpage.java
@@ -0,0 +1,13 @@
+package testpage;
+
+import page.loginpage;
+
+public class logintestpage {
+	public static void main(String[] args) {
+		loginpage lo=new loginpage();
+		lo.login();
+	}
+		
+	}
+
+
