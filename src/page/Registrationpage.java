982bf314662e667e8f3baf62e014090cad4471a9
Brindtha <jbrindtha@gmail.com>
2021-12-10 02:14:46 -0800

    Registration page added

---
+++ src/page/Registrationpage.java
@@ -0,0 +1,9 @@
+package page;
+
+public class Registrationpage {
+
+	
+
+	}
+
+
---
+++ src/testpage/Registrationtestpage.java
@@ -0,0 +1,13 @@
+package testpage;
+
+import page.loginpage;
+
+public class Registrationtestpage {
+	public static void main(String[] args) {
+		loginpage lo=new loginpage();
+		lo.login();
+	}
+		
+	}
+
+
